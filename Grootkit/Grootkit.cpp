c0a7c50531601c4e9ef260d895038937c7b9f23d
Grant Ogden <grant101.ogden@gmail.com>
2021-10-08 17:09:50 -0500

    EOD Lots of planning done

--- Grootkit/Grootkit.cpp
+++ Grootkit/Grootkit.cpp
@@ -1,8 +1,10 @@
 #include <ntddk.h>
+#include "GrootkitCommon.h"
 
 // prototypes
 void GrootkitUnload(PDRIVER_OBJECT DriverObject);
 NTSTATUS GrootkitCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+NTSTATUS GrootkitDeviceControl(PDEVICE_OBJECT DeviceObject, PIRP Irp);
 
 //Driver Entry
 extern "C" NTSTATUS
@@ -41,5 +43,40 @@ DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
 }
 
 void GrootkitUnload(PDRIVER_OBJECT DriverObject) {
+	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\Grootkit");
+	// delete symbolic link
+	IoDeleteSymbolicLink(&symLink);
 
+	// delete device object
+	IoDeleteDevice(DriverObject->DeviceObject);
+}
+
+NTSTATUS GrootkitCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+	UNREFERENCED_PARAMETER(DeviceObject);
+
+	Irp->IoStatus.Status = STATUS_SUCCESS;
+	Irp->IoStatus.Information = 0;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS GrootkitDeviceControl(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+	// get our IO_STACK_LOCATION
+	auto stack = IoGetCurrentIrpStackLocation(Irp);
+	auto status = STATUS_SUCCESS;
+
+	switch (stack->Parameters.DeviceIoControl.IoControlCode) {
+		case IOCTL_GROOTKIT:
+			// do the work
+			break;
+
+		default:
+			status = STATUS_INVALID_DEVICE_REQUEST;
+			break;
+	}
+
+	Irp->IoStatus.Status = status;
+	Irp->IoStatus.Information = 0;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+	return status;
 }
--- Grootkit/GrootkitCommon.h
+++ Grootkit/GrootkitCommon.h
@@ -5,6 +5,34 @@
 
 #define IOCTL_GROOTKIT CTL_CODE(GROOTKIT_DEVICE,0x800, METHOD_NEITHER, FILE_ANY_ACCESS)
 
-struct FileData {
-	UNICODE_STRING filePath;
+struct FileFilterData {
+	PUNICODE_STRING filePath;
 };
+
+struct FileCreationData {
+	PUNICODE_STRING filePath;
+	PUNICODE_STRING data;
+};
+
+struct ProcessFilterData {
+	UINT32 pid;
+};
+
+struct ProcessCreationData {
+	
+};
+
+union CommandData
+{
+	FileFilterData FileFilter;
+	FileCreationData FileCreation;
+	ProcessFilterData ProcessFilter;
+	ProcessCreationData ProcessCreation;
+};
+
+struct GrootkitData {
+	UINT16 whichCommand;
+	CommandData command;
+};
+
+
